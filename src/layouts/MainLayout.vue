632d84c3e97e0b85b902fe259172da7720b33da6
Jerry <jerry@list.ru>
2019-11-30 20:44:38 +0400

    added pages and layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="app-main-layout">
+    <!-- navbar -->
+    <nav>
+      <div class="nav-wrapper teal">
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+          <i class="material-icons">menu</i>
+        </a>
+        <div class="pl-5 navbar-left">
+          <ul class="black-text hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">Javascript</a>
+            </li>
+            <li>
+              <a href="mobile.html">Mobile</a>
+            </li>
+          </ul>
+        </div>
+
+        <ul class="right hide-on-small-and-down">
+          <li>
+            <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+              USER NAME
+              <i class="material-icons right">arrow_drop_down</i>
+            </a>
+
+            <ul id="dropdown" class="dropdown-content">
+              <li>
+                <a href="#" class="black-text">
+                  <i class="material-icons">account_circle</i>Профиль
+                </a>
+              </li>
+              <li class="divider" tabindex="-1"></li>
+              <li>
+                <a href="#" class="black-text">
+                  <i class="material-icons">assignment_return</i>Выйти
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- navbar end  -->
+    <!-- <nav class="navbar orange lighten-1">
+      <div class="nav-wrapper">
+        <div class="navbar-left">
+          <a href="#">
+            <i class="material-icons black-text">dehaze</i>
+          </a>
+          <span class="black-text">12.12.12</span>
+        </div>
+
+        <ul class="right hide-on-small-and-down">
+          <li>
+            <a
+                class="dropdown-trigger black-text"
+                href="#"
+                data-target="dropdown"
+            >
+              USER NAME
+              <i class="material-icons right">arrow_drop_down</i>
+            </a>
+
+            <ul id='dropdown' class='dropdown-content'>
+              <li>
+                <a href="#" class="black-text">
+                  <i class="material-icons">account_circle</i>Профиль
+                </a>
+              </li>
+              <li class="divider" tabindex="-1"></li>
+              <li>
+                <a href="#" class="black-text">
+                  <i class="material-icons">assignment_return</i>Выйти
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>-->
+
+    <ul class="sidenav" id="mobile-demo">
+      <li>
+        <a href="#" class="waves-effect waves-teal pointer">Счет</a>
+      </li>
+      <li>
+        <a href="#" class="waves-effect waves-orange pointer">История</a>
+      </li>
+      <li>
+        <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
+      </li>
+      <li>
+        <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
+      </li>
+      <li>
+        <a href="#" class="waves-effect waves-orange pointer">Категории</a>
+      </li>
+    </ul>
+
+    <main class="app-content">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,71 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [{
   path: '/',
   name: 'home',
-  component: Home
+  meta: {
+    layout: 'main'
+  },
+  component: () => import('../views/Home')
+}, {
+  path: '/categories',
+  name: 'categories',
+  meta: {
+    layout: 'main'
+  },
+  component: () => import('../views/Categories')
+}, {
+  path: '/detail',
+  name: 'detail',
+  meta: {
+    layout: 'main'
+  },
+  component: () => import('../views/Detail')
+}, {
+  path: '/history',
+  name: 'history',
+  meta: {
+    layout: 'main'
+  },
+  component: () => import('../views/History')
+}, {
+  path: '/planning',
+  name: 'planning',
+  meta: {
+    layout: 'main'
+  },
+  component: () => import('../views/Planning')
+}, {
+  path: '/profile',
+  name: 'profile',
+  meta: {
+    layout: 'main'
+  },
+  component: () => import('../views/Profile')
+}, {
+  path: '/record',
+  name: 'record',
+  meta: {
+    layout: 'main'
+  },
+  component: () => import('../views/Record')
+}, {
+  path: '/login',
+  name: 'login',
+  meta: {
+    layout: 'auth'
+  },
+  component: () => import('../views/Login')
+}, {
+  path: '/register',
+  name: 'register',
+  meta: {
+    layout: 'auth'
+  },
+  component: () => import('../views/Register')
 }]
 
 const router = new VueRouter({
