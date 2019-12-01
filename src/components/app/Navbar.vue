3f4d0ee903fea671301dd4cc479c380c2726aadd
Jerry <jerry@list.ru>
2019-12-01 10:30:49 +0400

    modified mainlayout navbar

---
+++ src/components/app/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav>
+    <div class="nav-wrapper teal">
+      <transition name="fade" mode="out-in">
+        <i
+          class="material-icons menu hide-on-large-only"
+          v-if="!show"
+          @click="$emit('click')"
+          key="menu"
+        >menu</i>
+        <i
+          class="material-icons clear hide-on-large-only"
+          v-else
+          @click="$emit('click')"
+          key="clear"
+        >clear</i>
+      </transition>
+      <div class="pl-5 navbar-left">
+        <ul class="black-text hide-on-med-and-down">
+          <router-link
+            v-for="link in links"
+            :key="link.url"
+            tag="li"
+            active-class="active"
+            :to="link.url"
+            :exact="link.exact"
+          >
+            <a href="#" class="waves-effect waves-teal pointer">{{link.title}}</a>
+          </router-link>
+        </ul>
+      </div>
+
+      <ul class="right">
+        <li>
+          <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+            USER NAME
+            <i class="material-icons right">arrow_drop_down</i>
+          </a>
+
+          <ul id="dropdown" class="dropdown-content">
+            <li>
+              <a href="#" class="black-text">
+                <i class="material-icons">account_circle</i>Профиль
+              </a>
+            </li>
+            <li class="divider" tabindex="-1"></li>
+            <li>
+              <a href="#" class="black-text">
+                <i class="material-icons">assignment_return</i>Выйти
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      { title: 'Счет', url: '/', exact: true },
+      { title: 'История', url: '/history' },
+      { title: 'Планирование', url: '/planning' },
+      { title: 'Новая запись', url: '/record' },
+      { title: 'Категории', url: '/categories' }
+    ],
+    show: false
+  })
+}
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,52 +1,7 @@
 <template>
   <div class="app-main-layout">
     <!-- navbar -->
-    <nav>
-      <div class="nav-wrapper teal">
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
-          <i class="material-icons">menu</i>
-        </a>
-        <div class="pl-5 navbar-left">
-          <ul class="black-text hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Sass</a>
-            </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">Javascript</a>
-            </li>
-            <li>
-              <a href="mobile.html">Mobile</a>
-            </li>
-          </ul>
-        </div>
-
-        <ul class="right hide-on-small-and-down">
-          <li>
-            <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
-              USER NAME
-              <i class="material-icons right">arrow_drop_down</i>
-            </a>
-
-            <ul id="dropdown" class="dropdown-content">
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">account_circle</i>Профиль
-                </a>
-              </li>
-              <li class="divider" tabindex="-1"></li>
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">assignment_return</i>Выйти
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar @click="show = !show" />
     <!-- navbar end  -->
     <!-- <nav class="navbar orange lighten-1">
       <div class="nav-wrapper">
@@ -85,25 +40,7 @@
         </ul>
       </div>
     </nav>-->
-
-    <ul class="sidenav" id="mobile-demo">
-      <li>
-        <a href="#" class="waves-effect waves-teal pointer">Счет</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">История</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Категории</a>
-      </li>
-    </ul>
-
+    <Sidebar v-model="show" />
     <main class="app-content">
       <div class="app-page">
         <router-view />
@@ -117,3 +54,18 @@
     </div>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/app/Navbar'
+import Sidebar from '@/components/app/Sidebar'
+export default {
+  name: 'main-layout',
+  data: () => ({
+    show: false
+  }),
+  components: {
+    Navbar,
+    Sidebar
+  }
+}
+</script>
