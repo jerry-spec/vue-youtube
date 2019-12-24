bdfe9c9629072e174558a1001279ee9c37102e0f
Jerry <jerry@mail.ru>
2019-12-24 10:48:04 +0400

    added firebase

--- src/main.js
+++ src/main.js
@@ -7,13 +7,33 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate)
 Vue.use(messagePlugin)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: 'AIzaSyCJbQJImnbugv6ME5gDYDt9q5FClMe2-_g',
+  authDomain: 'myvue-1993d.firebaseapp.com',
+  databaseURL: 'https://myvue-1993d.firebaseio.com',
+  projectId: 'myvue-1993d',
+  storageBucket: 'myvue-1993d.appspot.com',
+  messagingSenderId: '990451553681',
+  appId: '1:990451553681:web:1dfeee0d60811576eaf630'
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,27 +4,23 @@
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input id="email" type="text" v-model.trim="email"
-        :class="{ invalid:($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email) }" />
+          :class="{ invalid:($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email) }" />
         <label for="email">Email</label>
-        <small class="helper-text invalid"
-          v-if="$v.email.$dirty && !$v.email.required">
+        <small class="helper-text invalid" v-if="$v.email.$dirty && !$v.email.required">
           Поле Email не дожно быть пустым
         </small>
-        <small class="helper-text invalid"
-          v-else-if="$v.email.$dirty && !$v.email.email">
+        <small class="helper-text invalid" v-else-if="$v.email.$dirty && !$v.email.email">
           Введите корректный Email
         </small>
       </div>
       <div class="input-field">
         <input id="password" type="password" v-model.trim="password"
-        :class="{ invalid:($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength) }" />
+          :class="{ invalid:($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength) }" />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid"
-          v-if="$v.password.$dirty && !$v.password.required">
+        <small class="helper-text invalid" v-if="$v.password.$dirty && !$v.password.required">
           Введите пароль
         </small>
-        <small class="helper-text invalid"
-          v-else-if="$v.password.$dirty && !$v.password.minLength">
+        <small class="helper-text invalid" v-else-if="$v.password.$dirty && !$v.password.minLength">
           Пароль должен быть {{ $v.password.$params.minLength.min }} символов. Сейчас он {{password.length}}
         </small>
       </div>
@@ -46,7 +42,11 @@
 </template>
 
 <script>
-import { email, required, minLength } from 'vuelidate/lib/validators'
+import {
+  email,
+  required,
+  minLength
+} from 'vuelidate/lib/validators'
 import messages from '@/utils/messages'
 
 export default {
@@ -56,8 +56,14 @@ export default {
     password: ''
   }),
   validations: {
-    email: { email, required },
-    password: { required, minLength: minLength(6) }
+    email: {
+      email,
+      required
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
   },
   mounted () {
     if (messages[this.$route.query.message]) {
@@ -65,7 +71,7 @@ export default {
     }
   },
   methods: {
-    submitHandler () {
+    async submitHandler () {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -74,9 +80,10 @@ export default {
         email: this.email,
         password: this.password
       }
-
-      console.log(formData)
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
