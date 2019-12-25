fc0dd8f7119b3bbe04ab9b29ebb33a2aaf10f0ff
Jerry <jerry@mail.ru>
2019-12-25 11:25:24 +0400

    added registration by firebase

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
-  'logout': 'Вы вышли из системы'
+  'logout': 'Вы вышли из системы',
+  'auth/user-not-found': `<span style="color: red; font-weight: bold; font-family: Tahoma;"> Пользователя с таким email не существует!</span>`,
+  'auth/wrong-password': `<span style="color: red; font-weight: bold; font-family: Tahoma;">Неверный пароль!</span>`,
+  'auth/email-already-in-use': `<span style="color: red; font-weight: bold; font-family: Tahoma;">Email уже занят!</span>`
 }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -92,7 +92,7 @@ export default {
     agree: { checked: v => v }
   },
   methods: {
-    submitHandler () {
+    async submitHandler () {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -103,10 +103,10 @@ export default {
         password: this.password,
         name: this.name
       }
-
-      console.log(formData)
-
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
